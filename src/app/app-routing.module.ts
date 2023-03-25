0e5d59fc05a222a7fd319211cdbd9f40c1cb08ef
Rupamthakre <rupamthakre15@gmail.com>
2023-03-25 15:29:16 +0530

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomepageComponent } from './welcomepage/welcomepage.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ReportComponent } from './report/report.component';
+import { NotificationComponent } from './notification/notification.component';
+import { LeadComponent } from './lead/lead.component';
+import { MemberComponent } from './member/member.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomepageComponent },
+  { path: 'login', component : LoginComponent },
+  { path: 'register', component : RegisterComponent },
+  { path: 'dashboard', component : DashboardComponent },
+  { path: 'home', component : HomeComponent },
+  { path: 'profile', component : ProfileComponent },
+  { path: 'report', component : ReportComponent },
+  { path: 'notification', component : NotificationComponent },
+  { path: 'lead', component : LeadComponent },
+  { path: 'member', component : MemberComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomepageComponent } from './welcomepage/welcomepage.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ReportComponent } from './report/report.component';
+import { NotificationComponent } from './notification/notification.component';
+import { LeadComponent } from './lead/lead.component';
+import { MemberComponent } from './member/member.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomepageComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    HomeComponent,
+    ProfileComponent,
+    ReportComponent,
+    NotificationComponent,
+    LeadComponent,
+    MemberComponent
   ],
   imports: [
     BrowserModule,
